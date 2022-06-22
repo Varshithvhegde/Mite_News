c70d4cf550b67756799b02e4e94ff5a465289c31
varshithvhegde <varshithvh@gmail.com>
2022-06-22 20:58:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memeapp/Blog_Data.java
@@ -0,0 +1,47 @@
+package com.example.memeapp;
+
+import java.util.Map;
+
+public class Blog_Data {
+
+    private String caption;
+    private String username;
+    private String image;
+    private String timestamp;
+    public Blog_Data(){
+
+    }
+    public Blog_Data(String username, String caption, String image) {
+        this.username = username;
+        this.caption = caption;
+        this.image = image;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+//    public void setTimestamp(Map<String, String> timeStamp) {this.timestamp= timestamp;}
+//    public String getTimestamp() {return timestamp;}
+
+}
